796eaeba5d57f8509d4b2ba9d61013b3b6c2ef3f
Arpan Dhama <arpandhama2001@gmail.com>
2025-09-03 19:45:54 +0530

    Added: Authenticaiton in proxy requests

--- src/index.js
+++ src/index.js
@@ -3,38 +3,41 @@ const rateLimit = require("express-rate-limit");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const { checkAuth } = require("./middlewares/auth-request-middleware");
 
 const app = express();
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 2 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 15 * 60 * 1000, // 2 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
 
 // Creating Proxy
 app.use(
-    "/flightsService",
-    createProxyMiddleware({
-        target: ServerConfig.FLIGHT_SERVICE,
-        changeOrigin: true,
-        pathRewrite: { "^/flightsService": "/" },
-    })
+  "/flightsService",
+  checkAuth,
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/flightsService": "/" },
+  })
 );
 app.use(
-    "/bookingService",
-    createProxyMiddleware({
-        target: ServerConfig.BOOKING_SERVICE,
-        changeOrigin: true,
-    })
+  "/bookingService",
+  checkAuth,
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/bookingService": "/" },
+  })
 );
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Server is listening on port ${ServerConfig.PORT}`);
+  console.log(`Server is listening on port ${ServerConfig.PORT}`);
 });
